02a0203852595452e1d8e6f020a7c4bcd8c8f027
JohnDoe <gamefake@live.nl>
2020-04-20 18:40:01 +0200

    - Added editing towns

--- routes/towns.js
+++ routes/towns.js
@@ -12,13 +12,23 @@ router.get('/', function(req, res, next) {
         });
 });
 
+router.get('/:id', function(req, res, next){
+    models.Town.findAll({
+        where: {
+            id: req.params.id
+        }
+    }).then(town => {
+        res.json(town);
+    });
+});
+
 router.post('/', function(req, res, next) {
     const body = req.body;
     models.Town.create({
-        name: body.townName,
-        stability: body.townStability,
-        population: body.townPopulation,
-        support: body.townSupport
+        name: body.name,
+        stability: body.stability,
+        population: body.population,
+        support: body.support
     }).then(() => {
         res.redirect('/towns');
     });
@@ -32,6 +42,27 @@ router.post('/delete/:id', function(req, res, next) {
         }
     }).then(() => {
         res.redirect('/towns');
+    });
+});
+
+router.post('/edit', function(req, res, next) {
+    const id = req.body.id;
+    const name = req.body.name;
+    const stability = req.body.stability;
+    const population = req.body.population;
+    const support = req.body.support;
+
+    models.Town.update({
+        name: name,
+        stability: stability,
+        population: population,
+        support: support
+    },{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.send("OK");
     });
 });
 
